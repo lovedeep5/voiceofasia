683b8ffcca3f421a2759a787a8108927ebc0eadf
anuadroit <availapanti@gmail.com>
2018-08-17 02:30:41 +0530

    added welcome screen

---
+++ welcome.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width = device-width,initial-scale = 1">
+		<title>Welcome</title>
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+		<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+	<div class="container">
+		<div class="jumbotron">
+		<div class="signinform"></div>
+		<div>
+		<form class="text-center" id="frm_login">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 text-center pd-10">
+						<img src="images/Logo.png" alt="Logo" class="img-fluid">
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<input type="email" class="form-control" placeholder="Email Address">
+				<input type="password" class="form-control" placeholder="Password">
+			</div>
+			<div>
+				<a href="user-home.php" class="btn btn-success btn-block">Sign in</a>
+			</div>
+			<div class="facebook">
+				<a href="#" class="fab fa-facebook-f"></a> &nbsp;&nbsp;Sign in with Facebook
+			</div>
+			<div class="twitter">
+				<a href="#" class="fab fa-twitter"></a> &nbsp;Sign in with Twitter
+			</div>
+			<div>
+				<a href="#" style="color:white;float:left;">Forgot Password</a>
+				<a href="#" style="color:white;float:right;">Register</a>
+			</div>
+		</form>
+		</div>
+		</div>
+	</div>
+		
+	</body>
+</html>
