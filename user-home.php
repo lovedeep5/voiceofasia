0d98718d056bf4d6993210ebb98e2f0f9b209cfe
lovedeep5 <lovedeep5.abh@gmail.com>
2018-08-16 20:22:33 +0530

    New update 16.8.2018
    New Icons updated
    Search result page added

--- user-home.php
+++ user-home.php
@@ -1,4 +1,4 @@
-<?php require_once "header.php"; ?>
+  <?php require_once "header.php"; ?>
 <div class="page-content">
 <!-- adv start here -->
 <div class="container-fluid d-block d-lg-none d-xl-none d-md-none">
@@ -20,39 +20,39 @@
 <div class="container-fluid">
   <div class="row text-center justify-content-md-center justify-content-lg-center justify-content-xl-center services">
     <div class="col-4 col-xl-1 col-lg-1 col-md-1 col-sm-4 text-center">
-        <a href="#"><img src="images/cart-64.png" alt="message" class="img-responsive icon"></a>
+        <a href="#"><img src="images/challenges.png" alt="message" class="img-responsive service-icon"></a>
         <p class="text-black">Challenges</p>
     </div>
     <div class="col-4 col-xl-1 col-lg-1 col-md-1 col-sm-4">
-      <a href="#"><img src="images/cart-64.png" alt="message" class="img-responsive icon"></a>
+      <a href="#"><img src="images/industry.png" alt="message" class="img-responsive service-icon"></a>
       <p class="text-black">Industries</p>
     </div>
     <div class="col-4 col-xl-1 col-lg-1 col-md-1 col-sm-4">
-      <a href="#"><img src="images/cart-64.png" alt="message" class="img-responsive icon"></a>
+      <a href="#"><img src="images/job_roles.png" alt="message" class="img-responsive service-icon"></a>
       <p class="text-black">Job Roles</p>
     </div>
     <div class="col-4 col-xl-1 col-lg-1 col-md-1 col-sm-4">
-      <a href="#"><img src="images/cart-64.png" alt="message" class="img-responsive icon"></a>
+      <a href="#"><img src="images/skills.png" alt="message" class="img-responsive service-icon"></a>
       <p class="text-black">Skills</p>
     </div>
     <div class="col-4 col-xl-1 col-lg-1 col-md-1 col-sm-4">
-      <a href="#"><img src="images/cart-64.png" alt="message" class="img-responsive icon"></a>
+      <a href="#"><img src="images/workshops.png" alt="message" class="img-responsive service-icon"></a>
       <p class="text-black">Workshops</p>
     </div>
     <div class="col-4 col-xl-1 col-lg-1 col-md-1 col-sm-4">
-      <a href="#"><img src="images/cart-64.png" alt="message" class="img-responsive icon"></a>
+      <a href="#"><img src="images/projects.png" alt="message" class="img-responsive service-icon"></a>
       <p class="text-black">Projects</p>
     </div>
     <div class="col-4 col-xl-1 col-lg-1 col-md-1 col-sm-4">
-      <a href="#"><img src="images/cart-64.png" alt="message" class="img-responsive icon"></a>
+      <a href="#"><img src="images/content.png" alt="message" class="img-responsive service-icon"></a>
       <p class="text-black">Content</p>
     </div>
     <div class="col-4 col-xl-1 col-lg-1 col-md-1 col-sm-4">
-      <a href="#"><img src="images/cart-64.png" alt="message" class="img-responsive icon"></a>
+      <a href="#"><img src="images/ttc.png" alt="message" class="img-responsive service-icon"></a>
       <p class="text-black">TTC</p>
     </div>
     <div class="col-4 col-xl-1 col-lg-1 col-md-1 col-sm-4">
-      <a href="#"><img src="images/cart-64.png" alt="message" class="img-responsive icon"></a>
+      <a href="#"><img src="images/cart-64.png" alt="message" class="img-responsive service-icon"></a>
       <p class="text-black">Others</p>
     </div>
   </div>
@@ -82,7 +82,37 @@
         <div class="card-body">
           <h5 class="card-title">Jhon doe</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">View</a>
+          <a href="#" class="btn btn-primary size-w-100">View</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-6 col-xl-2 col-lg-2 col-md-2 col-sm-3 mt-10">
+      <div class="card" style="width: 100%;">
+        <img class="card-img-top" src="images/Untitled-3.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Jhon doe</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary size-w-100">View</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-6 col-xl-2 col-lg-2 col-md-2 col-sm-3 mt-10">
+      <div class="card" style="width: 100%;">
+        <img class="card-img-top" src="images/user-2.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Jhon doe</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary size-w-100">View</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-6 col-xl-2 col-lg-2 col-md-2 col-sm-3 mt-10">
+      <div class="card" style="width: 100%;">
+        <img class="card-img-top" src="images/user-1.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Jhon doe</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary size-w-100">View</a>
         </div>
       </div>
     </div>
@@ -92,7 +122,7 @@
         <div class="card-body">
           <h5 class="card-title">Jhon doe</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">View</a>
+          <a href="#" class="btn btn-primary size-w-100">View</a>
         </div>
       </div>
     </div>
@@ -102,43 +132,11 @@
         <div class="card-body">
           <h5 class="card-title">Jhon doe</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">View</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-6 col-xl-2 col-lg-2 col-md-2 col-sm-3 mt-10">
-      <div class="card" style="width: 100%;">
-        <img class="card-img-top" src="images/Untitled-2.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Jhon doe</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">View</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-6 col-xl-2 col-lg-2 col-md-2 col-sm-3 mt-10">
-      <div class="card" style="width: 100%;">
-        <img class="card-img-top" src="images/Untitled-2.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Jhon doe</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">View</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-6 col-xl-2 col-lg-2 col-md-2 col-sm-3 mt-10">
-      <div class="card" style="width: 100%;">
-        <img class="card-img-top" src="images/Untitled-2.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Jhon doe</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">View</a>
+          <a href="#" class="btn btn-primary size-w-100">View</a>
         </div>
       </div>
     </div>
   </div>
 </div>
 <!-- Recomended Profiles End here -->
-<p>add test</p>
-<p>add test</p>
 <?php require_once "footer.php"; ?>
